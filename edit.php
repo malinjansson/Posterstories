da59b160c0857727dae69661b21e94e48fe7121f
Malin Jansson <maalin.jaansson@gmail.com>
2025-03-24 14:51:34 +0100

    added a form

--- edit.php
+++ edit.php
@@ -17,11 +17,28 @@ require_once('Models/Product.php')
         if ($product == null){
             echo "<h1>Produkten finns inte<h1>";
         } else {
-            echo "<h1>$product->title<h1>";
-            echo "<p>$product->price<p>";
-            echo "<p>$product->stockLevel<p>";
-            echo "<p>$product->categoryName<p>";
+        }
+
+        if($_SERVER ['REQUEST_METHOD'] == 'POST') {
+            $product->title = $_POST['title'];
+            $product->price = $_POST['price'];
+            $product->stockLevel = $_POST['stockLevel'];
+            $product->categoryName = $_POST['categoryName'];
+            echo "<h1>The product has been updated</h1>";
+        } else{
+
         }
     ?>
+    <form method="POST">
+        <lable for="title">Title</lable>
+        <input type="text" name="title" value="<?php echo $product->title?>">
+        <lable for="title">Price</lable>
+        <input type="text" name="price" value="<?php echo $product->price?>">
+        <lable for="title">Stock</lable>
+        <input type="text" name="stockLevel" value="<?php echo $product->stockLevel?>">
+        <lable for="title">Category name</lable>
+        <input type="text" name="categoryName" value="<?php echo $product->categoryName?>">
+        <input type="submit" value="Update">
+    </form>
 </body>
 </html>
