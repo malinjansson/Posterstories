24cb44a48526bc14f5b11f475fe529455df96293
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-14 15:49:06 +0200

    changed webshop name

--- components/Footer.php
+++ components/Footer.php
@@ -2,7 +2,7 @@
     function Footer (){
     ?>
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Ivy Bloom's 2025</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Ivy Bloom's & Co. 2025</p></div>
         </footer>
     <?php
     }
