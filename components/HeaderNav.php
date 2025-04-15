3aad38cb80e9ee95ed9f1e23da5963d8625082d7
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-15 11:55:48 +0200

    changed to slug to catname

--- components/HeaderNav.php
+++ components/HeaderNav.php
@@ -16,7 +16,7 @@ require_once("Models/Database.php");
                                 <li><hr class="dropdown-divider" /></li>
                                 <?php
                                 foreach($dbConnection->getAllCategories() as $cat){
-                                    echo "<li><a class='dropdown-item' href='/category?categoryname=$cat'>$cat</a></li>";
+                                    echo "<li><a class='dropdown-item' href='/category?catname=$cat'>$cat</a></li>";
                                 }
                                 ?>
                                     <li><a class="dropdown-item" href="#!">En cat</a></li>
