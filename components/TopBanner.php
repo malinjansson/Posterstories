ae52cf304d25db34106be4952768e8121398d269
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-15 12:36:27 +0200

    styled topbanner

--- components/TopBanner.php
+++ components/TopBanner.php
@@ -1,11 +1,11 @@
 <?php
     function TopBanner (){
     ?>
-       <div class="card text-bg-dark">
-       <img src="assets/topbanner.jpg" class="topbanner-img" style="height: 90vh; object-fit: cover; object-position: top;" alt="Pink and white flower bouquets">
+       <div class="card p-0 rounded-0 m-0 w-100">
+       <img src="assets/topbanner.jpg" class="topbanner-img" alt="Pink and white flower bouquets">
             <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center text-center">
-                <h2 class="topbanner-title">Send a flourishing greeting</h2>
-                <a>Send flowers</a>
+                <h2 class="card-title">Send a flourishing greeting</h2>
+                <a class="btn btn-primary">Send flowers</a>
             </div>
         </div>
     <?php
