59e1a208fb88307d5484838df61f225b57d176ca
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-17 11:34:05 +0200

    created the productcard section as a components and put it in on the homepage and categorypage

--- pages/home.php
+++ pages/home.php
@@ -3,6 +3,7 @@ require_once("Models/Product.php");
 require_once("components/HeadLinks.php");
 require_once("components/HeaderNav.php");
 require_once("components/TopBanner.php");
+require_once("components/ProductCards.php");
 require_once("components/Footer.php");
 require_once("Models/Database.php");
 
@@ -17,7 +18,7 @@ $dbConnection = new Database();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Shop Homepage - Start Bootstrap Template</title>
+        <title>Ivy Bloom's & Co. - Send flowers</title>
         <?php HeadLinks()?>
     </head>
     <body>
@@ -26,43 +27,7 @@ $dbConnection = new Database();
         <!-- Header-->
         <?php TopBanner()?>
         <!-- Section-->
-        <section class="py-5">
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <?php
-                    foreach($dbConnection->getAllProducts() as $prod) {
-                    ?>
-                        <div class="col mb-5">
-                            <div class="card h-100">
-                                <?php if($prod->price < 10) { ?>
-                                    <!-- Sale badge-->
-                                     <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                                <?php } ?>
-                                <!-- Product image-->
-                                <img class="card-img-top" src="<?php echo $prod->img; ?>" alt="..." />
-                                <!-- Product details-->
-                                <div class="card-body p-4">
-                                    <div class="text-center">
-                                        <!-- Product name-->
-                                        <h5 class="fw-bolder"><?php echo $prod->title;?></h5>
-                                        <!-- Product teaser-->
-                                        <p class="fst-italic"><?php echo $prod->teaser;?></>
-                                    </div>
-                                    <div class="text-center">
-                                           <!-- Product price-->
-                                            $<?php echo $prod->price?>.00
-                                    </div>
-                                </div>
-                                <!-- Product actions-->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                                </div>
-                            </div>
-                        </div> 
-                    <?php } ?>  
-                </div>
-            </div> 
-        </section>
+        <?php ProductCards()?>
         <!-- Footer-->
         <?php Footer()?>
         <!-- Bootstrap core JS-->
