5d8c6c1246a34bc7b7c9a47e95d72e263f3c0417
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-24 13:40:56 +0200

    changed title

--- pages/delete.php
+++ pages/delete.php
@@ -25,7 +25,7 @@
          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
          <meta name="description" content="" />
          <meta name="author" content="" />
-         <title>Shop Homepage - Start Bootstrap Template</title>
+         <title>Poster stories</title>
          <?php HeadLinks()?>
      </head>
  <body>
--- pages/new.php
+++ pages/new.php
@@ -28,7 +28,7 @@ require_once("Models/Database.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Poster stories</title>
     <?php HeadLinks()?>
 </head>
 <body>
