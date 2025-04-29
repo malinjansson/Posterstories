ba0f71cf0fcd56a4859fc24d9c3730ac8c3efc82
Malin Jansson <maalin.jaansson@gmail.com>
2025-04-29 09:19:00 +0200

    updated Cart class

--- Models/Cart.php
+++ Models/Cart.php
@@ -1,39 +1,67 @@
-Cart-php
 <?php
+
 class Cart {
-     private $dbContext;
-     private $sessionId;
-     private $userId;
-     private $cartItems = [];
-     public function __construct($dbContext, $sessionId, $userId = null) {
-         $this->dbContext = $dbContext;
-         $this->session_id = $sessionId;
-         $this->userId = $userId;
-         $this->cartItems = $this->dbContext->getCartItems($userId,$sessionId);
-     }
-     public function addItem($productId, $quantity) {
-         if (isset($this->cartItems[$productId])) {
-             $this->cartItems[$productId] += $quantity;
-         } else {
-             $this->cartItems[$productId] = $quantity;
-         }
-         $this->dbContext->updateCartItem($this->userId,$this->sessionId,  $productId, $this->cartItems[$productId]);
-     }
-     public function removeItem($productId, $quantity) {
-         if (isset($this->cartItems[$productId])) {
-             $this->cartItems[$productId] -= $quantity;
-             $this->dbContext->updateCartItem($this->userId,$this->sessionId, $productId, $this->cartItems[$productId]);
-             if ($this->cartItems[$productId] <= 0) {
-                 unset($this->cartItems[$productId]);
-             } 
-         }
-     }
-     public function getItems() {
-         return $this->cartItems;
-     }
-     public function clearCart() {
-         $this->cartItems = [];
-     }
+    private $dbContext;
+    private $sessionId;
+    private $userId;
+    private $cartItems = [];
+
+    public function __construct($dbContext, $sessionId, $userId = null) {
+        $this->dbContext = $dbContext;
+        $this->session_id = $sessionId;
+        $this->userId = $userId;
+        $this->cartItems = $this->dbContext->getCartItems($userId,$sessionId);
+
+    }
+
+    public function convertSessionToUser($userId, $newSessionId) {
+        $this->dbContext->convertSessionToUser($this->sessionId, $userId, $newSessionId);
+      
+        $this->userId = $userId;
+        $this->sessionId = $newSessionId;
+    }
+
+    public function addItem($productId, $quantity) {
+        $item = $this->getCartItem($productId);
+        if (!$item) {
+            $item = new CartItem();
+            $item->productId = $productId;
+            $item->quantity = $quantity;
+            array_push($this->cartItems, $item);
+        }else{
+            $item->quantity += $quantity;
+        }
+        $this->dbContext->updateCartItem($this->userId,$this->sessionId, $productId, $item->quantity);
+    }
+
+    public function removeItem($productId, $quantity) {
+        $item = $this->getCartItem($productId);
+        if( !$item) {
+            return;
+        }
+        $item->quantity -= $quantity;
+        $this->dbContext->updateCartItem($this->userId,$this->sessionId, $productId, $item->quantity);
+        if ($item->quantity <= 0) {
+            array_splice($this->cartItems, array_search($item, $this->cartItems), 1);
+        }
+    }
+
+    public function getCartItem($productId) {
+        foreach ($this->cartItems as $item) {
+            if ($item->productId == $productId) {
+                return $item;
+            }
+        }
+        return null;
+    }
+
+    public function getItems() {
+        return $this->cartItems;
+    }
+
+    public function clearCart() {
+        $this->cartItems = [];
+    }
 }
 
 ?>
