1f2ec0cc5dbe749373f913fbb16c9d5c180662e2
Malin Jansson <maalin.jaansson@gmail.com>
2025-06-10 10:39:24 +0200

    fixed checkoutSuccess errors

--- pages/checkoutSuccess.php
+++ pages/checkoutSuccess.php
@@ -1,15 +1,17 @@
 <?php
 require_once('Models/Product.php');
-require_once("components/Footer.php");
 require_once('Models/Database.php');
 require_once("Models/Cart.php");
+require_once("components/Footer.php");
+require_once("components/HeaderNav.php");
+require_once("components/HeadLinks.php");
 
 $dbConnection = new Database();
 
 $userId = null;
 $session_id = null;
 
-if($dbContext->getUsersDatabase()->getAuth()->isLoggedIn()){
+if($dbConnection->getUsersDatabase()->getAuth()->isLoggedIn()){
     $userId = $dbConnection->getUsersDatabase()->getAuth()->getUserId();
 }
 
@@ -22,15 +24,6 @@ $cart = new Cart($dbConnection, $session_id, $userId);
 <!DOCTYPE html>
 <html lang="en">
     <head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5NXP0GE5CV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-5NXP0GE5CV',{ 'debug_mode':true });
-</script>
-
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
@@ -39,32 +32,6 @@ $cart = new Cart($dbConnection, $session_id, $userId);
         <?php HeadLinks()?>
     </head>
 <body>
-    <?php 
-    
-    $googleItems = [];
-    foreach($cart->getItems() as $cartitem){
-        array_push($googleItems, [
-            
-            "quantity" => $cartitem->quantity,
-            "price" =>$cartitem->price,
-            "item_id"=>$cartitem->id,
-            "item_name"=>$cartitem->productName,
-        ]);
-    }
-    
-    ?>
-
-<script>
-gtag("event", "purchase", {
-    transaction_id:Math.floor(Math.random() * 99999999),
-  currency: "SEK",
-  value: <?php echo $cart->getTotalPrice(); ?>,
-  items: [
-    <?php echo json_encode($googleItems); ?>
-  ]
-});
-</script>
-
 <?php HeaderNav()?>
     <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
